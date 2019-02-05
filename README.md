0b95f85471ed7e83e41429e00d76b0de5ac56537
MoNsTeR <zgm_striker17@hotmail.com>
2019-02-05 18:59:49 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# My-Fitness-Calc
-A calculator for determining fitness level using React
+# myFitCalc
+A calculator for determining and tracking fitness level using React.
+
+## Screenshots
+![](./client/public/screenshot3.png)
+![](./client/public/screenshot1.png)
+![](./client/public/screenshot2.png)
+
+ ## Tech/framework used
+React, Mongoose/MongoDB, React-Google-Login, Semantic-UI-React, Express, Node.js, Moment.js, Axios, Recharts, FontAwesome, Google-Maps-React
+
+ ## Installation
+If you want to try it out locally, fork the repo, clone your forked repository, and run the following in the terminal of the root directory:
+```sh
+yarn install && yarn start
+```
+
+ ## Contribute
+If you would like to contribute, please open an issue or submit a pull request.
+
+ ## Author
+**Zacharias George Miles**
--- client/src/components/Gym-Finder/Map.js
+++ client/src/components/Gym-Finder/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react'
 
 const mapStyles = {
   width: '100vw',
@@ -12,7 +12,7 @@ export class MapContainer extends Component {
     activeMarker: {},          //Shows the active marker upon click
     selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
   };
-  onMarkerClick = (props, marker, e) => 
+  onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
@@ -28,27 +28,27 @@ export class MapContainer extends Component {
     }
   };
   render() {
-    if(sessionStorage.getItem('userData') === null) window.location.replace('/');
+    if (sessionStorage.getItem('userData') === null) window.location.replace('/');
     return (
-        <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
-          initialCenter={{ lat: 33.749, lng: -84.388 }}
-            >
-            <Marker
-              onClick={this.onMarkerClick}
-              name={'Georgia State Capitol'}
-            />
-            <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}
-              onClose={this.onClose}
-            >
-              <div>
-                <h4 style={{'color': 'black'}}>{this.state.selectedPlace.name}</h4>
-              </div>
-            </InfoWindow>
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{ lat: 33.749, lng: -84.388 }}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Georgia State Capitol'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4 style={{ 'color': 'black' }}>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
